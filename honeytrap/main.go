55ac3020622cc28e720607637167c965e5140ab5
EngAladdin <xengaladdin@gmail.com>
2026-04-20 04:58:08 +0200

    Refactor module and update dependencies in main.go
    Updated module declaration and added dependencies.

--- honeytrap/main.go
+++ honeytrap/main.go
@@ -1,31 +1,29 @@
-// Copyright 2016-2019 DutchSec (https://dutchsec.com/)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package main
+module github.com/honeytrap/honeytrap
 
-import (
-	"math/rand"
-	"os"
-	"time"
+go 1.12
 
-	"github.com/honeytrap/honeytrap/cmd/honeytrap"
+require (
+	github.com/BurntSushi/toml v0.3.0
+	github.com/Shopify/sarama v1.16.0
+	github.com/boltdb/bolt v1.3.1
+	github.com/cenkalti/backoff/v4 v4.0.0
+	github.com/dgraph-io/badger v0.0.0-20180227002726-94594b20babf
+	github.com/dutchcoders/gobus v0.0.0-20180915095724-ece5a7810d96
+	github.com/elastic/go-lumber v0.1.0
+	github.com/fatih/color v1.6.0
+	github.com/fuyufjh/splunk-hec-go v0.3.3
+	github.com/glycerine/rbuf v0.0.0-20171031012212-54320fe9f6f3
+	github.com/google/gopacket v1.1.14
+	github.com/gorilla/websocket v1.2.0
+	github.com/honeytrap/protocol v0.0.0-20190410072324-219b95413db0
+	github.com/mimoo/disco v0.0.0-20180114190844-15dd4b8476c9
+	github.com/op/go-logging v0.0.0-20160211212156-b2cb9fa56473
+	github.com/pkg/profile v1.2.1
+	github.com/rs/xid v0.0.0-20170604230408-02dd45c33376
+	github.com/satori/go.uuid v1.2.0
+	github.com/songgao/packets v0.0.0-20160404182456-549a10cd4091
+	github.com/songgao/water v0.0.0-20180221190335-75f112d19d5a
+	github.com/streadway/amqp v0.0.0-20180315184602-8e4aba63da9f
+	gopkg.in/olivere/elastic.v5 v5.0.65
+	gopkg.in/urfave/cli.v1 v1.20.0
 )
-
-func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-}
-
-func main() {
-	app := honeytrap.New()
-	app.Run(os.Args)
-}
